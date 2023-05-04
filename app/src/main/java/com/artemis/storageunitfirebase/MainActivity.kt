a2072e9db34bb63fd876cacfae13093f1a869ba8
elmomoloco7890 <thegreek.god@gmail.com>
2023-05-04 00:44:19 -0700

    Cool beans at work in the storage

--- app/src/main/java/com/artemis/storageunitfirebase/MainActivity.kt
+++ app/src/main/java/com/artemis/storageunitfirebase/MainActivity.kt
@@ -2,10 +2,42 @@ package com.artemis.storageunitfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.artemis.storageunitfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = mainBinding
+        setContentView(mainBinding.root)
+
+        //toolbar
+        val toolBar = binding.toolbar
+        toolBar.title = ""
+        setSupportActionBar(binding.toolbar)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.storage_unit_nav_host) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
